ec37f91ac00c807eba8c0462dc973411c69f723c
AnonymTVde <thephil2006@gmail.com>
2024-09-17 21:05:59 +0200

    Dateien von Moodel geholt und .h in .hpp geändert

--- main.cpp
+++ main.cpp
@@ -1,129 +1,81 @@
+/* 
+* main.cpp
+*
+* Created: 12.02.2023 22:37:38
+* Author: Thomas Heß
+*/
+
 /* mbed Microcontroller Library
  * Copyright (c) 2019 ARM Limited
  * SPDX-License-Identifier: Apache-2.0
  */
+#undef __ARM_FP
 
 #include "mbed.h"
+#include "Automat.hpp"
+#include "Hardware_Methoden.hpp"  // für Zugriff auf die Hardware (GPIO, UART, ADC, ...) z.B. für Polling
+#include "Variable_Global.hpp"  // Definition der globalen Variablen, die von anderen Funktionen in anderen cpp-files benötigt werden
 #include <cstdint>
+#include <cstdio>
 
-#define RECHTS true
-#define LINKS false
 
-DigitalOut A(PC_3);
-DigitalOut a(PC_2);
-DigitalOut B(PC_1);
-DigitalOut b(PC_0);
 
-int Bestromung_rechts[4][4] = 
-                                {
-                                    {0,1,1,0}, //6
-                                    {0,1,0,1}, //5 
-                                    {1,0,0,1}, //9
-                                    {1,0,1,0}  //A
-                                };
-int state;
-bool richtung; // false links. true rechts
-void init();
+/************************************************************
+ * Angeschlossene Hardware:
+ *
+ * Tasten:      Rechtslauf   PB_4     extInt, steigende Flanke
+ *              Stopp:       PB_3     extInt, steigende Flanke
+                Linkslauf:   PB_0     extInt, steigende Flanke
+ *
+ *
+ *
+ * 
+ * Motor:       Anschluss A:    PC_3     
+ *              Anschluss A/:   PC_2     
+ *              Anschluss B:    PC_1
+ *              Anschluss B/:   PC_0
+ *
+ * Tasten am Dabble-Gamepad (Digital) mit zugehörigem 2Byte-Hex-Code
+ * Pfeil nach oben:     0x0001
+ * Pfeil nach unten     0x0002
+ * Pfeil nach links     0x0004
+ * Pfeil nach rechts    0x0008
+ * Start                0x0100
+ * Select               0x0200
+ * Dreieck              0x0400
+ * Kreis                0x0800
+ * Kreuz                0x1000
+ * Quadrat              0x2000
 
-void init_TIM6(void);
-void isr_TIM6(void);
-void stopp_TIM6(void);
-void start_TIM6(void);
+ ************************************************************/
 
-void schritt_rechts(void);
-void schritt_links(void);
+Automat derAutomat;  // Deklaration der globalen Variable derAutomat, die in Variable_Global.h extern definiert ist
+                            // nur in einem cpp-file deklarieren, in den anderen cpp-files nur per 
+                            // #include "Globals.h" verfügbar machen
+                            // Im Konstruktor von Automat wird init() aufgerufen um die FSM zu initialisieren
 
-void init()
-{
-    state = 0;
-    richtung = RECHTS;
-    A.write(Bestromung_rechts[state][0]);
-    a.write(Bestromung_rechts[state][1]);
-    B.write(Bestromung_rechts[state][2]);
-    b.write(Bestromung_rechts[state][3]);
-}
-
-void init_TIM6()
-{
-    //Timer mit Takt versorgen
-    RCC->APB1ENR |= 0b10000;
-
-    //AutoRelodRegister auf 250ms
-    TIM6->ARR = 250;
-
-    //Precaler einstellen auf 1ms
-    TIM6->PSC = 31999;
-
-    //Zähler auf Wert Setzten
-    TIM6->CNT = 0;
-
-    //Überlaufflag löschen
-    TIM6->SR = 0;
-
-    //ISR in die VactorTabelle eintagen
-    NVIC_SetVector(TIM6_IRQn, (uint32_t) &isr_TIM6);
-
-    //Inertupt freigeben NIVC
-    HAL_NVIC_EnableIRQ(TIM6_IRQn);
-
-    //Interrupt Freigen
-    TIM6->DIER = 1;
-
-    //glogabe freigebe
-    __enable_irq();
-}
-
-void isr_TIM6()
-{
-    if (richtung == RECHTS)
-    {
-        schritt_rechts();
-    }
-    else
-    {
-        schritt_links();
-    }
-    TIM6->SR = 0;
-}
-
-void stopp_TIM6()
-{
-    TIM6->CR1 = 0;
-}
-
-void start_TIM6()
-{
-    TIM6->CR1 = 1;
-}
-
-void schritt_rechts()
-{
-    state++;
-    state %= 4;
-    A.write(Bestromung_rechts[state][0]);
-    a.write(Bestromung_rechts[state][1]);
-    B.write(Bestromung_rechts[state][2]);
-    b.write(Bestromung_rechts[state][3]);
-}
-
-void schritt_links()
-{
-    state += 3;
-    state %= 4;
-    A.write(Bestromung_rechts[state][0]);
-    a.write(Bestromung_rechts[state][1]);
-    B.write(Bestromung_rechts[state][2]);
-    b.write(Bestromung_rechts[state][3]);
-}
 
 int main()
-{ 
-    init();
-    init_TIM6();
-    start_TIM6();
+{
+       
+	
+    
 
-    while (true)
-    {
-        
-    }
+	 // Globale Interruptfreigabe
+	 __enable_irq();
+
+
+    while (true) {
+
+            
+            
+
+    
+
+
+    derAutomat.process_StateChange();
+
+    } // of while(true)
+
+    return 1;
 }
